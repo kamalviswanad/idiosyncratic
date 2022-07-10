cbd3f1519e91f573b2eff7daa45531c679147c2b
KamalViswanad <100687186+KamalViswanad@users.noreply.github.com>
2022-07-10 12:20:49 +0530

    Code

--- src/Idiosyncratic.php
+++ src/Idiosyncratic.php
@@ -2,8 +2,10 @@
 function Idiosyncratic($name,$separator){
 $UpperCase="QWERTYUIOPLKJHGFDSAZXCVBNMQWERTYUIOPLKJHGFDSAZXCVBNMQWERTYUIOPLKJHGFDSAZXCVBNM";
 $LowerCase="qwertyuioplkjhgfdsazxcvbnmqwertyuioplkjhgfdsazxcvbnmqwertyuioplkjhgfdsazxcvbnm";
+$LowerCase2="mnbvcxzasdfghjklpoiuytrewq";
 $UpperCaseShuffle= str_shuffle($UpperCase);
 $LowerCaseShuffle= str_shuffle($LowerCase);
+$LowerCase2Shuffle= str_shuffle($LowerCase2);
 $year=date("Y");
 $seconds=date("s");
 $rand=rand(0,10000);
@@ -18,7 +20,6 @@ $t="0".$rand;
 }else if(strlen($rand)==5){
  $t=$rand;
 }
-$ID=$name.$separator.substr($year,2,4).$seconds.substr(microtime(),3,3).substr($UpperCaseShuffle,0,1).rand(1,9).$t.substr($LowerCaseShuffle,0,3).rand(0,9).substr($UpperCaseShuffle,3,4).rand(100,999).substr($LowerCaseShuffle,6,1);
+$ID=$name.$separator.substr($year,2,4).$seconds.substr(microtime(),3,3).substr($UpperCaseShuffle,0,1).rand(1,9).$t.substr($LowerCaseShuffle,0,3).rand(0,9).substr($UpperCaseShuffle,3,4).rand(100,999).substr($LowerCase2Shuffle,6,1).rand(1,9);
 echo $ID;
-}
 ?>
